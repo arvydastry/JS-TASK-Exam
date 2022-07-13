936f4896442a162ceec61c2dfc5f00ef60886dbd
arvydastry <arvydas.trybe@gmail.com>
2022-07-13 19:56:45 +0300

    Atlikta trečia užduotis

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,53 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+const renderCard = (users) => {
+  const { login, avatar_url } = users;
+
+  const card = document.createElement("div");
+
+  const output = document.getElementById("output");
+
+  const loginEl = document.createElement("h3");
+
+  const avatarEl = document.createElement("h5");
+
+  card.className = "card";
+
+  loginEl.textContent = login;
+
+  avatarEl.textContent = avatar_url;
+
+  card.append(loginEl, avatarEl);
+
+  card.style.backgroundColor = "lightblue";
+
+  output.append(card);
+};
+
+const messageGone = document.getElementById("message");
+
+btn.addEventListener("click", function handleClick() {
+  if (messageGone.style.display === "none") {
+    messageGone.style.display = "block";
+
+    btn.textContent = "Hide element";
+  } else {
+    messageGone.style.display = "none";
+
+    btn.textContent = "Show element";
+  }
+});
+
+fetch("https://api.github.com/users")
+  .then((resp) => resp.json())
+
+  .then((response) => {
+    response.forEach((users) => renderCard(users));
+  })
+
+  .catch((error) => {
+    console.error(error, ": failed to load");
+  });
