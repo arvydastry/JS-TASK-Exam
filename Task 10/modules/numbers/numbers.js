8ae615c03897750392905911a5e88c4782faf117
arvydastry <arvydas.trybe@gmail.com>
2022-07-14 20:47:49 +0300

    10 užduotis neišspręsta, nerandu sprendimo

--- Task 10/modules/numbers/numbers.js
+++ Task 10/modules/numbers/numbers.js
@@ -1,5 +1,9 @@
-let one = 1;
-let two = 2;
-let three = 3;
-let four = 4;
-let five = 5;
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export let one = 1;
+export let two = 2;
+export let three = 3;
+export let four = 4;
+export let five = 5;
+
+export default numbers;
--- Task 10/script.js
+++ Task 10/script.js
@@ -1,6 +1,11 @@
 /* ------------------------------ TASK 10 ---------------------------------------------------
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
+  import numbers { one, two, three, four, five } from "./modules/numbers/numbers.js";
+  import composition, { one, four } from "./modules/math/comosition.js";
+  import division, { four, two } from "./modules/math/division";
+  import substraction, { three, two } from "./modules/math/subtraction";
+  import multiplication, { five, two } from "./modules/math/multiplication";
 
 let a = composition(one, four);
 let b = division(four, two);
